625a99ca709f08f4505c3400f05c5a4c232e710d
Illouminus <professionnel.baillot@gmail.com>
2025-01-08 15:49:14 +0100

    adding method for extension check

--- src/Responder.cpp
+++ src/Responder.cpp
@@ -27,3 +27,27 @@ const LocationConfig *Responder::findLocation(const ServerConfig &srv, const std
 	}
 	return bestLoc;
 }
+
+std::string Responder::getContentTypeByExtension(const std::string &path)
+{
+	// Находим последний '.'
+	size_t pos = path.rfind('.');
+	if (pos == std::string::npos)
+		return "application/octet-stream";
+	std::string ext = path.substr(pos);
+	// В зависимости от расширения
+	if (ext == ".html" || ext == ".htm")
+		return "text/html";
+	if (ext == ".css")
+		return "text/css";
+	if (ext == ".js")
+		return "application/javascript";
+	if (ext == ".jpg" || ext == ".jpeg")
+		return "image/jpeg";
+	if (ext == ".png")
+		return "image/png";
+	if (ext == ".gif")
+		return "image/gif";
+	// Можно добавить ещё
+	return "application/octet-stream";
+}
