03dcf94fb6051e8c16095284d030007aa363ab91
Illouminus <professionnel.baillot@gmail.com>
2025-01-08 15:48:07 +0100

    adding find location method at Responder class

--- src/Responder.cpp
+++ src/Responder.cpp
@@ -1,9 +1,13 @@
 #include "Responder.hpp"
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <sys/stat.h> // для проверки директории
 
 Responder::Responder() {};
 Responder::~Responder() {};
 
-const LocationConfig *Responder::findLocation(const ServerConfig &srv, std::string const &path)
+const LocationConfig *Responder::findLocation(const ServerConfig &srv, const std::string &path)
 {
 	size_t bestMatch = 0;
 	const LocationConfig *bestLoc = NULL;
@@ -11,16 +15,15 @@ const LocationConfig *Responder::findLocation(const ServerConfig &srv, std::stri
 	for (size_t i = 0; i < srv.locations.size(); i++)
 	{
 		const std::string &locPath = srv.locations[i].path;
+		// Сравнение префикса:
 		if (path.compare(0, locPath.size(), locPath) == 0)
 		{
-			// значит path начинается на locPath
 			if (locPath.size() > bestMatch)
 			{
 				bestMatch = locPath.size();
-				bestLoc = &(srv.locations[i]);
+				bestLoc = &srv.locations[i];
 			}
 		}
 	}
-
 	return bestLoc;
 }
